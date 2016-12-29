76a6ae246a38ea3e0514f23a8a31990052bd2949
sudhanshu <sudhanshuccp@gmail.com>
2016-12-29 13:16:38 +0530

    funtions complete

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import get_content
+from .views import get_content,post_content
 
 
 urlpatterns = [
-url(r'^getinfo/',get_content)
+url(r'^getinfo/',get_content),
+url(r'^postcontent/',post_content)
 ]
--- cars/views.py
+++ cars/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render,get_object_or_404
 from .models import Car
 from django.http import HttpResponse
 import json
+from .forms import PostForm
 
 # Create your views here.
 def get_content(request):
 	p=request.GET.get('instance','')
 	if(p==''):
-		p='12'
+		p='14'
 	k=get_object_or_404(Car,Instance=p)
 	context={
 	'Model':k.Model,
@@ -19,6 +20,22 @@ def get_content(request):
 	}
 	#context=
 	return HttpResponse(json.dumps(context), content_type="application/json")
+
+def post_content(request):
+	if(request.method=='GET'):
+		form=PostForm()
+		context={
+		"form":form
+		}
+		return render(request,"cars/post_form.html",context)
+	if(request.method=='POST'):
+		form=PostForm(request.POST)
+		if(form.is_valid()):
+			f=form.save(commit=False)
+			f.save()
+			return HttpResponse('OK')
+		else:
+			return render(request,"cars/post_form.html",{"form":form})
 
 
 
